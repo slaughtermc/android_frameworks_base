d8476f31e33f4cfeae077c3e661c36eb847dcd15
maxwen <max.weninger@gmail.com>
2019-12-13 19:43:13 +0100

    Navbar: Arrow keys while typing resurrection
    Change-Id: Idd87c6451cfd359fc97be07e308465e5205fad59

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5894,6 +5894,15 @@ public final class Settings {
         public static final String THEME_TILE_ENABLED_MODE = "theme_tile_enabled_mode";
 
         /**
+         * Whether to show arrow keys in navigation bar
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_ARROW_KEYS = "navigation_bar_menu_arrow_keys";
+
+        private static final Validator NAVIGATION_BAR_ARROW_KEYS_VALIDATOR =
+                BOOLEAN_VALIDATOR;
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -6020,7 +6029,8 @@ public final class Settings {
             LOCK_SHOW_STATUS_BAR,
             QS_BLUR_INTENSITY,
             TEXT_CLOCK_ALIGNMENT,
-            DOUBLE_TAP_SLEEP_GESTURE
+            DOUBLE_TAP_SLEEP_GESTURE,
+            NAVIGATION_BAR_ARROW_KEYS,
         };
 
         /**
@@ -6224,6 +6234,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(LOCK_SHOW_STATUS_BAR);
             PRIVATE_SETTINGS.add(TEXT_CLOCK_ALIGNMENT);
             PRIVATE_SETTINGS.add(INCALL_FEEDBACK_VIBRATE);
+            PRIVATE_SETTINGS.add(NAVIGATION_BAR_ARROW_KEYS);
         }
 
         /**
@@ -6388,6 +6399,8 @@ public final class Settings {
             VALIDATORS.put(QS_BLUR_INTENSITY, QS_BLUR_INTENSITY_VALIDATOR);
             VALIDATORS.put(TEXT_CLOCK_ALIGNMENT, TEXT_CLOCK_ALIGNMENT_VALIDATOR);
             VALIDATORS.put(INCALL_FEEDBACK_VIBRATE, INCALL_FEEDBACK_VIBRATE_VALIDATOR);
+            VALIDATORS.put(NAVIGATION_BAR_ARROW_KEYS,
+                    NAVIGATION_BAR_ARROW_KEYS_VALIDATOR);
         }
 
         /**
